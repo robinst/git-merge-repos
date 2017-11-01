8841567ff0b0cdd452ebfd714864dc182278a0cb
Parker Williams <parker.williams@duethealth.com>
2017-10-31 21:34:02 -0400

    all errors gone

--- src/main/java/com/ameriod/git/merger/RepoMerger.java
+++ src/main/java/com/ameriod/git/merger/RepoMerger.java
@@ -37,233 +37,232 @@ import org.eclipse.jgit.transport.RefSpec;
  */
 public class RepoMerger {
 
-	private final List<SubtreeConfig> subtreeConfigs;
-	private final Repository repository;
+    private final List<SubtreeConfig> subtreeConfigs;
+    private final Repository repository;
 
-	public RepoMerger(String outputRepositoryPath,
-			List<SubtreeConfig> subtreeConfigs) throws IOException {
-		this.subtreeConfigs = subtreeConfigs;
-		File file = new File(outputRepositoryPath);
-		repository = new RepositoryBuilder().setWorkTree(file).build();
-		if (!repository.getDirectory().exists()) {
-			repository.create();
-		}
-	}
+    public RepoMerger(String outputRepositoryPath,
+                      List<SubtreeConfig> subtreeConfigs) throws IOException {
+        this.subtreeConfigs = subtreeConfigs;
+        File file = new File(outputRepositoryPath);
+        repository = new RepositoryBuilder().setWorkTree(file).build();
+        if (!repository.getDirectory().exists()) {
+            repository.create();
+        }
+    }
 
-	public List<MergedRef> run() throws IOException, GitAPIException {
-		fetch();
-		List<MergedRef> mergedBranches = mergeBranches();
-		List<MergedRef> mergedTags = mergeTags();
-		List<MergedRef> mergedRefs = new ArrayList<>();
-		mergedRefs.addAll(mergedBranches);
-		mergedRefs.addAll(mergedTags);
-		deleteOriginalRefs();
-		resetToBranch();
-		return mergedRefs;
-	}
+    public List<MergedRef> run() throws IOException, GitAPIException {
+        fetch();
+        List<MergedRef> mergedBranches = mergeBranches();
+        List<MergedRef> mergedTags = mergeTags();
+        List<MergedRef> mergedRefs = new ArrayList<>();
+        mergedRefs.addAll(mergedBranches);
+        mergedRefs.addAll(mergedTags);
+        deleteOriginalRefs();
+        resetToBranch();
+        return mergedRefs;
+    }
 
-	private void fetch() throws GitAPIException {
-		for (SubtreeConfig config : subtreeConfigs) {
-			RefSpec branchesSpec = new RefSpec(
-					"refs/heads/*:refs/heads/original/"
-							+ config.getRemoteName() + "/*");
-			RefSpec tagsSpec = new RefSpec("refs/tags/*:refs/tags/original/"
-					+ config.getRemoteName() + "/*");
-			Git git = new Git(repository);
-			git.fetch().setRemote(config.getFetchUri().toPrivateString())
-					.setRefSpecs(branchesSpec, tagsSpec).call();
-		}
-	}
+    private void fetch() throws GitAPIException {
+        for (SubtreeConfig config : subtreeConfigs) {
+            RefSpec branchesSpec = new RefSpec(
+                    "refs/heads/*:refs/heads/original/"
+                            + config.getRemoteName() + "/*");
+            RefSpec tagsSpec = new RefSpec("refs/tags/*:refs/tags/original/"
+                    + config.getRemoteName() + "/*");
+            Git git = new Git(repository);
+            git.fetch().setRemote(config.getFetchUri().toPrivateString())
+                    .setRefSpecs(branchesSpec, tagsSpec).call();
+        }
+    }
 
-	private List<MergedRef> mergeBranches() throws IOException {
-		List<MergedRef> mergedRefs = new ArrayList<>();
-		Collection<String> branches = getRefSet("refs/heads/original/");
-		for (String branch : branches) {
-			MergedRef mergedBranch = mergeBranch(branch);
-			mergedRefs.add(mergedBranch);
-		}
-		return mergedRefs;
-	}
+    private List<MergedRef> mergeBranches() throws IOException {
+        List<MergedRef> mergedRefs = new ArrayList<>();
+        Collection<String> branches = getRefSet("refs/heads/original/");
+        for (String branch : branches) {
+            MergedRef mergedBranch = mergeBranch(branch);
+            mergedRefs.add(mergedBranch);
+        }
+        return mergedRefs;
+    }
 
-	private List<MergedRef> mergeTags() throws IOException {
-		List<MergedRef> mergedRefs = new ArrayList<>();
-		Collection<String> tags = getRefSet("refs/tags/original/");
-		for (String tag : tags) {
-			MergedRef mergedTag = mergeTag(tag);
-			mergedRefs.add(mergedTag);
-		}
-		return mergedRefs;
-	}
+    private List<MergedRef> mergeTags() throws IOException {
+        List<MergedRef> mergedRefs = new ArrayList<>();
+        Collection<String> tags = getRefSet("refs/tags/original/");
+        for (String tag : tags) {
+            MergedRef mergedTag = mergeTag(tag);
+            mergedRefs.add(mergedTag);
+        }
+        return mergedRefs;
+    }
 
-	private void deleteOriginalRefs() throws IOException {
-		try (RevWalk revWalk = new RevWalk(repository)) {
-			Collection<Ref> refs = new ArrayList<>();
-			RefDatabase refDatabase = repository.getRefDatabase();
-			Map<String, Ref> originalBranches = refDatabase.getRefs("refs/heads/original/");
-			Map<String, Ref> originalTags = refDatabase.getRefs("refs/tags/original/");
-			refs.addAll(originalBranches.values());
-			refs.addAll(originalTags.values());
-			for (Ref originalRef : refs) {
-				RefUpdate refUpdate = repository.updateRef(originalRef.getName());
-				refUpdate.setForceUpdate(true);
-				refUpdate.delete(revWalk);
-			}
-		}
-	}
+    private void deleteOriginalRefs() throws IOException {
+        RevWalk revWalk = new RevWalk(repository);
+        Collection<Ref> refs = new ArrayList<>();
+        RefDatabase refDatabase = repository.getRefDatabase();
+        Map<String, Ref> originalBranches = refDatabase.getRefs("refs/heads/original/");
+        Map<String, Ref> originalTags = refDatabase.getRefs("refs/tags/original/");
+        refs.addAll(originalBranches.values());
+        refs.addAll(originalTags.values());
+        for (Ref originalRef : refs) {
+            RefUpdate refUpdate = repository.updateRef(originalRef.getName());
+            refUpdate.setForceUpdate(true);
+            refUpdate.delete(revWalk);
+        }
 
-	private void resetToBranch() throws IOException, GitAPIException {
-		Ref master = repository.getRef(Constants.R_HEADS + "master");
-		if (master != null) {
-			Git git = new Git(repository);
-			git.reset().setMode(ResetType.HARD).setRef(master.getName()).call();
-		}
-	}
+    }
 
-	private MergedRef mergeBranch(String branch) throws IOException {
+    private void resetToBranch() throws IOException, GitAPIException {
+        Ref master = repository.getRef(Constants.R_HEADS + "master");
+        if (master != null) {
+            Git git = new Git(repository);
+            git.reset().setMode(ResetType.HARD).setRef(master.getName()).call();
+        }
+    }
 
-		Map<SubtreeConfig, ObjectId> resolvedRefs = resolveRefs(
-				"refs/heads/original/", branch);
+    private MergedRef mergeBranch(String branch) throws IOException {
 
-		Map<SubtreeConfig, RevCommit> parentCommits = new LinkedHashMap<>();
-		try (RevWalk revWalk = new RevWalk(repository)) {
-			for (SubtreeConfig config : subtreeConfigs) {
-				ObjectId objectId = resolvedRefs.get(config);
-				if (objectId != null) {
-					RevCommit commit = revWalk.parseCommit(objectId);
-					parentCommits.put(config, commit);
-				}
-			}
-		}
+        Map<SubtreeConfig, ObjectId> resolvedRefs = resolveRefs(
+                "refs/heads/original/", branch);
 
-		MergedRef mergedRef = getMergedRef("branch", branch, parentCommits.keySet());
+        Map<SubtreeConfig, RevCommit> parentCommits = new LinkedHashMap<>();
+        RevWalk revWalk = new RevWalk(repository);
+        for (SubtreeConfig config : subtreeConfigs) {
+            ObjectId objectId = resolvedRefs.get(config);
+            if (objectId != null) {
+                RevCommit commit = revWalk.parseCommit(objectId);
+                parentCommits.put(config, commit);
+            }
+        }
 
-		ObjectId mergeCommit = new SubtreeMerger(repository).createMergeCommit(parentCommits,
-				mergedRef.getMessage());
+        MergedRef mergedRef = getMergedRef("branch", branch, parentCommits.keySet());
 
-		RefUpdate refUpdate = repository.updateRef("refs/heads/" + branch);
-		refUpdate.setNewObjectId(mergeCommit);
-		refUpdate.update();
+        ObjectId mergeCommit = new SubtreeMerger(repository).createMergeCommit(parentCommits,
+                mergedRef.getMessage());
 
-		return mergedRef;
-	}
+        RefUpdate refUpdate = repository.updateRef("refs/heads/" + branch);
+        refUpdate.setNewObjectId(mergeCommit);
+        refUpdate.update();
 
-	private MergedRef mergeTag(String tagName) throws IOException {
-		Map<SubtreeConfig, ObjectId> resolvedRefs = resolveRefs(
-				"refs/tags/original/", tagName);
+        return mergedRef;
+    }
 
-		// Annotated tag that should be used for creating the merged tag, null
-		// if only lightweight tags exist
-		RevTag referenceTag = null;
-		Map<SubtreeConfig, RevCommit> parentCommits = new LinkedHashMap<>();
+    private MergedRef mergeTag(String tagName) throws IOException {
+        Map<SubtreeConfig, ObjectId> resolvedRefs = resolveRefs(
+                "refs/tags/original/", tagName);
 
-		try (RevWalk revWalk = new RevWalk(repository)) {
-			for (Map.Entry<SubtreeConfig, ObjectId> entry : resolvedRefs
-					.entrySet()) {
-				SubtreeConfig config = entry.getKey();
-				ObjectId objectId = entry.getValue();
-				RevCommit commit;
-				RevObject revObject = revWalk.parseAny(objectId);
-				if (revObject instanceof RevCommit) {
-					// Lightweight tag (ref points directly to commit)
-					commit = (RevCommit) revObject;
-				} else if (revObject instanceof RevTag) {
-					// Annotated tag (ref points to tag object with message,
-					// which in turn points to commit)
-					RevTag tag = (RevTag) revObject;
-					RevObject peeled = revWalk.peel(tag);
-					if (peeled instanceof RevCommit) {
-						commit = (RevCommit) peeled;
+        // Annotated tag that should be used for creating the merged tag, null
+        // if only lightweight tags exist
+        RevTag referenceTag = null;
+        Map<SubtreeConfig, RevCommit> parentCommits = new LinkedHashMap<>();
 
-						if (referenceTag == null) {
-							referenceTag = tag;
-						} else {
-							// We already have one, but use the last (latest)
-							// tag as reference
-							PersonIdent referenceTagger = referenceTag.getTaggerIdent();
-							PersonIdent thisTagger = tag.getTaggerIdent();
-							if (thisTagger != null && referenceTagger != null
-									&& thisTagger.getWhen().after(referenceTagger.getWhen())) {
-								referenceTag = tag;
-							}
-						}
-					} else {
-						String msg = "Peeled tag " + tag.getTagName()
-								+ " does not point to a commit, but to the following object: "
-								+ peeled;
-						throw new IllegalStateException(msg);
-					}
-				} else {
-					throw new IllegalArgumentException("Object with ID "
-							+ objectId + " has invalid type for a tag: "
-							+ revObject);
-				}
-				parentCommits.put(config, commit);
-			}
-		}
+        RevWalk revWalk = new RevWalk(repository);
+        for (Map.Entry<SubtreeConfig, ObjectId> entry : resolvedRefs
+                .entrySet()) {
+            SubtreeConfig config = entry.getKey();
+            ObjectId objectId = entry.getValue();
+            RevCommit commit;
+            RevObject revObject = revWalk.parseAny(objectId);
+            if (revObject instanceof RevCommit) {
+                // Lightweight tag (ref points directly to commit)
+                commit = (RevCommit) revObject;
+            } else if (revObject instanceof RevTag) {
+                // Annotated tag (ref points to tag object with message,
+                // which in turn points to commit)
+                RevTag tag = (RevTag) revObject;
+                RevObject peeled = revWalk.peel(tag);
+                if (peeled instanceof RevCommit) {
+                    commit = (RevCommit) peeled;
 
-		MergedRef mergedRef = getMergedRef("tag", tagName, parentCommits.keySet());
-		ObjectId mergeCommit = new SubtreeMerger(repository).createMergeCommit(parentCommits,
-				mergedRef.getMessage());
+                    if (referenceTag == null) {
+                        referenceTag = tag;
+                    } else {
+                        // We already have one, but use the last (latest)
+                        // tag as reference
+                        PersonIdent referenceTagger = referenceTag.getTaggerIdent();
+                        PersonIdent thisTagger = tag.getTaggerIdent();
+                        if (thisTagger != null && referenceTagger != null
+                                && thisTagger.getWhen().after(referenceTagger.getWhen())) {
+                            referenceTag = tag;
+                        }
+                    }
+                } else {
+                    String msg = "Peeled tag " + tag.getTagName()
+                            + " does not point to a commit, but to the following object: "
+                            + peeled;
+                    throw new IllegalStateException(msg);
+                }
+            } else {
+                throw new IllegalArgumentException("Object with ID "
+                        + objectId + " has invalid type for a tag: "
+                        + revObject);
+            }
+            parentCommits.put(config, commit);
+        }
 
-		ObjectId objectToReference;
-		if (referenceTag != null) {
-			TagBuilder tagBuilder = new TagBuilder();
-			tagBuilder.setTag(tagName);
-			tagBuilder.setMessage(referenceTag.getFullMessage());
-			tagBuilder.setTagger(referenceTag.getTaggerIdent());
-			tagBuilder.setObjectId(mergeCommit, Constants.OBJ_COMMIT);
-			try (ObjectInserter inserter = repository.newObjectInserter()) {
-				objectToReference = inserter.insert(tagBuilder);
-				inserter.flush();
-			}
-		} else {
-			objectToReference = mergeCommit;
-		}
 
-		String ref = Constants.R_TAGS + tagName;
-		RefUpdate refUpdate = repository.updateRef(ref);
-		refUpdate.setExpectedOldObjectId(ObjectId.zeroId());
-		refUpdate.setNewObjectId(objectToReference);
-		Result result = refUpdate.update();
-		if (result != Result.NEW) {
-			throw new IllegalStateException("Creating tag ref " + ref + " for "
-					+ objectToReference + " failed with result " + result);
-		}
+        MergedRef mergedRef = getMergedRef("tag", tagName, parentCommits.keySet());
+        ObjectId mergeCommit = new SubtreeMerger(repository).createMergeCommit(parentCommits,
+                mergedRef.getMessage());
 
-		return mergedRef;
-	}
+        ObjectId objectToReference;
+        if (referenceTag != null) {
+            TagBuilder tagBuilder = new TagBuilder();
+            tagBuilder.setTag(tagName);
+            tagBuilder.setMessage(referenceTag.getFullMessage());
+            tagBuilder.setTagger(referenceTag.getTaggerIdent());
+            tagBuilder.setObjectId(mergeCommit, Constants.OBJ_COMMIT);
+            ObjectInserter inserter = repository.newObjectInserter();
+            objectToReference = inserter.insert(tagBuilder);
+            inserter.flush();
 
-	private Collection<String> getRefSet(String prefix) throws IOException {
-		Map<String, Ref> refs = repository.getRefDatabase().getRefs(prefix);
-		TreeSet<String> result = new TreeSet<>();
-		for (String refName : refs.keySet()) {
-			String branch = refName.split("/", 2)[1];
-			result.add(branch);
-		}
-		return result;
-	}
+        } else {
+            objectToReference = mergeCommit;
+        }
 
-	private Map<SubtreeConfig, ObjectId> resolveRefs(String refPrefix,
-			String name) throws IOException {
-		Map<SubtreeConfig, ObjectId> result = new LinkedHashMap<>();
-		for (SubtreeConfig config : subtreeConfigs) {
-			String repositoryName = config.getRemoteName();
-			String remoteBranch = refPrefix + repositoryName + "/" + name;
-			ObjectId objectId = repository.resolve(remoteBranch);
-			if (objectId != null) {
-				result.put(config, objectId);
-			}
-		}
-		return result;
-	}
+        String ref = Constants.R_TAGS + tagName;
+        RefUpdate refUpdate = repository.updateRef(ref);
+        refUpdate.setExpectedOldObjectId(ObjectId.zeroId());
+        refUpdate.setNewObjectId(objectToReference);
+        Result result = refUpdate.update();
+        if (result != Result.NEW) {
+            throw new IllegalStateException("Creating tag ref " + ref + " for "
+                    + objectToReference + " failed with result " + result);
+        }
 
-	private MergedRef getMergedRef(String refType, String refName,
-			Set<SubtreeConfig> configsWithRef) {
-		LinkedHashSet<SubtreeConfig> configsWithoutRef = new LinkedHashSet<>(
-				subtreeConfigs);
-		configsWithoutRef.removeAll(configsWithRef);
+        return mergedRef;
+    }
 
-		return new MergedRef(refType, refName, configsWithRef, configsWithoutRef);
-	}
+    private Collection<String> getRefSet(String prefix) throws IOException {
+        Map<String, Ref> refs = repository.getRefDatabase().getRefs(prefix);
+        TreeSet<String> result = new TreeSet<>();
+        for (String refName : refs.keySet()) {
+            String branch = refName.split("/", 2)[1];
+            result.add(branch);
+        }
+        return result;
+    }
+
+    private Map<SubtreeConfig, ObjectId> resolveRefs(String refPrefix,
+                                                     String name) throws IOException {
+        Map<SubtreeConfig, ObjectId> result = new LinkedHashMap<>();
+        for (SubtreeConfig config : subtreeConfigs) {
+            String repositoryName = config.getRemoteName();
+            String remoteBranch = refPrefix + repositoryName + "/" + name;
+            ObjectId objectId = repository.resolve(remoteBranch);
+            if (objectId != null) {
+                result.put(config, objectId);
+            }
+        }
+        return result;
+    }
+
+    private MergedRef getMergedRef(String refType, String refName,
+                                   Set<SubtreeConfig> configsWithRef) {
+        LinkedHashSet<SubtreeConfig> configsWithoutRef = new LinkedHashSet<>(
+                subtreeConfigs);
+        configsWithoutRef.removeAll(configsWithRef);
+
+        return new MergedRef(refType, refName, configsWithRef, configsWithoutRef);
+    }
 
 }
--- src/main/java/com/ameriod/git/merger/SubtreeMerger.java
+++ src/main/java/com/ameriod/git/merger/SubtreeMerger.java
@@ -31,110 +31,109 @@ import org.eclipse.jgit.util.RawParseUtils;
  */
 public class SubtreeMerger {
 
-	private final Repository repository;
+    private final Repository repository;
 
-	public SubtreeMerger(Repository repository) {
-		this.repository = repository;
-	}
+    public SubtreeMerger(Repository repository) {
+        this.repository = repository;
+    }
 
-	public ObjectId createMergeCommit(Map<SubtreeConfig, RevCommit> parentCommits, String message)
-			throws IOException {
-		PersonIdent latestIdent = getLatestPersonIdent(parentCommits.values());
-		DirCache treeDirCache = createTreeDirCache(parentCommits, message);
-		List<? extends ObjectId> parentIds = new ArrayList<>(parentCommits.values());
-		try (ObjectInserter inserter = repository.newObjectInserter()) {
-			ObjectId treeId = treeDirCache.writeTree(inserter);
+    public ObjectId createMergeCommit(Map<SubtreeConfig, RevCommit> parentCommits, String message)
+            throws IOException {
+        PersonIdent latestIdent = getLatestPersonIdent(parentCommits.values());
+        DirCache treeDirCache = createTreeDirCache(parentCommits, message);
+        List<? extends ObjectId> parentIds = new ArrayList<>(parentCommits.values());
+        ObjectInserter inserter = repository.newObjectInserter();
+        ObjectId treeId = treeDirCache.writeTree(inserter);
 
-			PersonIdent repositoryUser = new PersonIdent(repository);
-			PersonIdent ident = new PersonIdent(repositoryUser, latestIdent.getWhen().getTime(),
-					latestIdent.getTimeZoneOffset());
-			CommitBuilder commitBuilder = new CommitBuilder();
-			commitBuilder.setTreeId(treeId);
-			commitBuilder.setAuthor(ident);
-			commitBuilder.setCommitter(ident);
-			commitBuilder.setMessage(message);
-			commitBuilder.setParentIds(parentIds);
-			ObjectId mergeCommit = inserter.insert(commitBuilder);
-			inserter.flush();
-			return mergeCommit;
-		}
-	}
+        PersonIdent repositoryUser = new PersonIdent(repository);
+        PersonIdent ident = new PersonIdent(repositoryUser, latestIdent.getWhen().getTime(),
+                latestIdent.getTimeZoneOffset());
+        CommitBuilder commitBuilder = new CommitBuilder();
+        commitBuilder.setTreeId(treeId);
+        commitBuilder.setAuthor(ident);
+        commitBuilder.setCommitter(ident);
+        commitBuilder.setMessage(message);
+        commitBuilder.setParentIds(parentIds);
+        ObjectId mergeCommit = inserter.insert(commitBuilder);
+        inserter.flush();
+        return mergeCommit;
 
-	private PersonIdent getLatestPersonIdent(Collection<RevCommit> commits) {
-		PersonIdent latest = null;
-		for (RevCommit commit : commits) {
-			PersonIdent ident = commit.getCommitterIdent();
-			Date when = ident.getWhen();
-			if (latest == null || when.after(latest.getWhen())) {
-				latest = ident;
-			}
-		}
-		return latest;
-	}
+    }
 
-	private DirCache createTreeDirCache(Map<SubtreeConfig, RevCommit> parentCommits,
-			String commitMessage) throws IOException {
+    private PersonIdent getLatestPersonIdent(Collection<RevCommit> commits) {
+        PersonIdent latest = null;
+        for (RevCommit commit : commits) {
+            PersonIdent ident = commit.getCommitterIdent();
+            Date when = ident.getWhen();
+            if (latest == null || when.after(latest.getWhen())) {
+                latest = ident;
+            }
+        }
+        return latest;
+    }
 
-		try (TreeWalk treeWalk = new TreeWalk(repository)) {
-			treeWalk.setRecursive(true);
-			addTrees(parentCommits, treeWalk);
+    private DirCache createTreeDirCache(Map<SubtreeConfig, RevCommit> parentCommits,
+                                        String commitMessage) throws IOException {
 
-			DirCacheBuilder builder = DirCache.newInCore().builder();
-			while (treeWalk.next()) {
-				AbstractTreeIterator iterator = getSingleTreeIterator(treeWalk, commitMessage);
-				if (iterator == null) {
-					throw new IllegalStateException(
-							"Tree walker did not return a single tree (should not happen): "
-									+ treeWalk.getPathString());
-				}
-				byte[] path = Arrays.copyOf(iterator.getEntryPathBuffer(),
-						iterator.getEntryPathLength());
-				DirCacheEntry entry = new DirCacheEntry(path);
-				entry.setFileMode(iterator.getEntryFileMode());
-				entry.setObjectId(iterator.getEntryObjectId());
-				builder.add(entry);
-			}
-			builder.finish();
-			return builder.getDirCache();
-		}
-	}
+        TreeWalk treeWalk = new TreeWalk(repository);
+        treeWalk.setRecursive(true);
+        addTrees(parentCommits, treeWalk);
 
-	private void addTrees(Map<SubtreeConfig, RevCommit> parentCommits, TreeWalk treeWalk)
-			throws IOException {
-		for (Map.Entry<SubtreeConfig, RevCommit> entry : parentCommits.entrySet()) {
-			String directory = entry.getKey().getSubtreeDirectory();
-			RevCommit parentCommit = entry.getValue();
-			if (".".equals(directory)) {
-				treeWalk.addTree(parentCommit.getTree());
-			} else {
-				byte[] prefix = directory.getBytes(RawParseUtils.UTF8_CHARSET);
-				CanonicalTreeParser treeParser = new CanonicalTreeParser(prefix,
-						treeWalk.getObjectReader(), parentCommit.getTree());
-				treeWalk.addTree(treeParser);
-			}
-		}
-	}
+        DirCacheBuilder builder = DirCache.newInCore().builder();
+        while (treeWalk.next()) {
+            AbstractTreeIterator iterator = getSingleTreeIterator(treeWalk, commitMessage);
+            if (iterator == null) {
+                throw new IllegalStateException(
+                        "Tree walker did not return a single tree (should not happen): "
+                                + treeWalk.getPathString());
+            }
+            byte[] path = Arrays.copyOf(iterator.getEntryPathBuffer(),
+                    iterator.getEntryPathLength());
+            DirCacheEntry entry = new DirCacheEntry(path);
+            entry.setFileMode(iterator.getEntryFileMode());
+            entry.setObjectId(iterator.getEntryObjectId());
+            builder.add(entry);
+        }
+        builder.finish();
+        return builder.getDirCache();
+    }
 
-	private AbstractTreeIterator getSingleTreeIterator(TreeWalk treeWalk, String commitMessage) {
-		AbstractTreeIterator result = null;
-		int treeCount = treeWalk.getTreeCount();
-		for (int i = 0; i < treeCount; i++) {
-			AbstractTreeIterator it = treeWalk.getTree(i, AbstractTreeIterator.class);
-			if (it != null) {
-				if (result != null) {
-					String msg = "Trees of repositories overlap in path '"
-							+ it.getEntryPathString()
-							+ "'. "
-							+ "We can only merge non-overlapping trees, "
-							+ "so make sure the repositories have been prepared for that. "
-							+ "One possible way is to process each repository to move the root to a subdirectory first.\n"
-							+ "Current commit:\n" + commitMessage;
-					throw new IllegalStateException(msg);
-				} else {
-					result = it;
-				}
-			}
-		}
-		return result;
-	}
+    private void addTrees(Map<SubtreeConfig, RevCommit> parentCommits, TreeWalk treeWalk)
+            throws IOException {
+        for (Map.Entry<SubtreeConfig, RevCommit> entry : parentCommits.entrySet()) {
+            String directory = entry.getKey().getSubtreeDirectory();
+            RevCommit parentCommit = entry.getValue();
+            if (".".equals(directory)) {
+                treeWalk.addTree(parentCommit.getTree());
+            } else {
+                byte[] prefix = directory.getBytes(RawParseUtils.UTF8_CHARSET);
+                CanonicalTreeParser treeParser = new CanonicalTreeParser(prefix,
+                        treeWalk.getObjectReader(), parentCommit.getTree());
+                treeWalk.addTree(treeParser);
+            }
+        }
+    }
+
+    private AbstractTreeIterator getSingleTreeIterator(TreeWalk treeWalk, String commitMessage) {
+        AbstractTreeIterator result = null;
+        int treeCount = treeWalk.getTreeCount();
+        for (int i = 0; i < treeCount; i++) {
+            AbstractTreeIterator it = treeWalk.getTree(i, AbstractTreeIterator.class);
+            if (it != null) {
+                if (result != null) {
+                    String msg = "Trees of repositories overlap in path '"
+                            + it.getEntryPathString()
+                            + "'. "
+                            + "We can only merge non-overlapping trees, "
+                            + "so make sure the repositories have been prepared for that. "
+                            + "One possible way is to process each repository to move the root to a subdirectory first.\n"
+                            + "Current commit:\n" + commitMessage;
+                    throw new IllegalStateException(msg);
+                } else {
+                    result = it;
+                }
+            }
+        }
+        return result;
+    }
 }
