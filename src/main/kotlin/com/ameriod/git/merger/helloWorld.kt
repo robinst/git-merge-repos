2c9f72ba9557915f2227e2bcf52309171325716f
Parker Williams <parker.williams@duethealth.com>
2017-10-31 21:22:09 -0400

    first commit, copied the hello world sample app

---
+++ src/main/kotlin/com/ameriod/git/merger/helloWorld.kt
@@ -0,0 +1,13 @@
+package com.ameriod.git.merger
+
+fun getGreeting(): String {
+    val words = mutableListOf<String>()
+    words.add("Hello,")
+    words.add("world!")
+
+    return words.joinToString(separator = " ")
+}
+
+fun main(args: Array<String>) {
+    println(getGreeting())
+}
---
+++ src/test/kotlin/com/ameriod/git/merger/tests.kt
@@ -0,0 +1,10 @@
+package com.ameriod.git.merger
+
+import kotlin.test.assertEquals
+import org.junit.Test
+
+class TestSource {
+    @Test fun f() {
+        assertEquals("Hello, world!", getGreeting())
+    }
+}
